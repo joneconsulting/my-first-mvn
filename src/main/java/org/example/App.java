89ce9a69360783db76b436f365234e4117bc79a1
Kenneth Lee <edowon0623@gmail.com>
2023-08-24 16:45:41 +0900

    계산기 코드 추가, 테스트 코드 추가

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,19 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+
+        // 계산기 사용 예제(덧셈)
+//        Calculator calc = new Calculator();
+//        int result = calc.add(5, 10);
+//        System.out.println("Result=" + result);
+//        System.out.println(10 + 20);
+//        System.out.println("Hi" + "there");
+//        System.out.println("Hi" + " " + "there");
+//        String name = "이도원";
+//        System.out.println("Hi" + name);
+//        System.out.println("10" + 20);
     }
 }
